309caa3da5b5d51c2ded2913d3b1fcabe185805b
davidrondina <rondinadavid.r@gmail.com>
2024-10-11 18:47:32 +0800

    chore: install tailwind-typography, inertia-modal, moment

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
-        './node_modules/@inertiaui/modal-vue/src/**/*.{js,vue}', // To style Inertia Modal
+        './node_modules/@inertiaui/modal-vue/src/**/*.{js,vue}',
     ],
 
     theme: {
@@ -22,5 +22,34 @@ export default {
     plugins: [
         forms,
         require('daisyui'),
+        require('@tailwindcss/typography'),
     ],
+
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+
+                    "primary": "#003eff",
+
+                    "secondary": "#009f00",
+
+                    "accent": "#0086ff",
+
+                    "neutral": "#091319",
+
+                    "base-100": "#fffaff",
+
+                    "info": "#7ec9ff",
+
+                    "success": "#00cb8b",
+
+                    "warning": "#ffc900",
+
+                    "error": "#f50030",
+                },
+            },
+        ],
+    },
+
 };
