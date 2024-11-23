d3a71bebf616e415f6501d7256e72787fd278db7
shuraiken <realninjadude@gmail.com>
2024-11-23 18:57:12 +0800

    add emit

--- resources/js/Components/Form/SelectInput.vue
+++ resources/js/Components/Form/SelectInput.vue
@@ -1,21 +1,29 @@
 <script setup>
 defineProps({
     placeholder: {
-        default: 'Select an option',
-        type: String
+        default: "Select an option",
+        type: String,
     },
     choices: {
         type: [Array, Object],
         required: false,
-    }
-})
+    },
+});
+
+const emits = defineEmits(["update-value"]);
 </script>
 
 <template>
-    <select v-model="model" class="text-gray-900 border-gray-300 select focus:border-primary focus:ring-primary rounded-xl">
+    <select
+        @change="emits('update-value')"
+        v-model="model"
+        class="text-gray-900 border-gray-300 select focus:border-primary focus:ring-primary rounded-xl"
+    >
         <option disabled>{{ placeholder }}</option>
         <template v-if="choices">
-            <option v-for="choice in choices" :value="choice">{{ choice }}</option>
+            <option v-for="choice in choices" :value="choice">
+                {{ choice }}
+            </option>
         </template>
 
         <!-- Additional options -->
