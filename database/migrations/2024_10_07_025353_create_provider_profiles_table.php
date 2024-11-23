8456b3c35e8f7986b4612349ff2d55d61c663184
shuraiken <realninjadude@gmail.com>
2024-11-23 16:26:16 +0800

    update services & provider profiles migration file

--- database/migrations/2024_10_07_025353_create_provider_profiles_table.php
+++ database/migrations/2024_10_07_025353_create_provider_profiles_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->string('service_type');
             $table->string('experience');
             $table->string('contact');
-            $table->string('verified_at')->nullable();
+            $table->dateTime('verified_at')->nullable();
             $table->string('certificate');
             $table->foreignIdFor(Profile::class)->constrained()->onDelete('cascade');
             $table->timestamps();
--- database/migrations/2024_10_07_025749_create_services_table.php
+++ database/migrations/2024_10_07_025749_create_services_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,8 +15,17 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->float('price');
+            $table->string('thumbnail')->nullable();
+            $table->tinyInteger('price_type')
+                ->comment('fixed or duration');
             $table->longText('description');
-            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->longText('terms_and_conditions')->nullable();
+            $table->boolean('is_approved');
+            $table->tinyInteger('service_type');
+            $table->tinyInteger('barangay_id');
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
