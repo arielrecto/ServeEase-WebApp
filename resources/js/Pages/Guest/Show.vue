55f0b99de0529fd1c868f19339514b4e7357f262
shuraiken <realninjadude@gmail.com>
2024-12-21 03:10:52 +0800

    service details page for guests wip

--- resources/js/Pages/Guest/Show.vue
+++ resources/js/Pages/Guest/Show.vue
@@ -1,31 +1,59 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import Header from '@/Components/Guest/Header.vue';
-import Footer from '@/Components/Footer.vue';
+import { Head, Link } from "@inertiajs/vue3";
+import Header from "@/Components/Guest/Header.vue";
+import Footer from "@/Components/Footer.vue";
 
-defineProps(['service']);
+defineProps(["service", 'canLogin', 'canRegister']);
 </script>
 
 <template>
     <Head title="Services" />
 
-    <div class="min-h-screen bg-gray-100">
+    <div class="bg-gray-100">
         <Header :canLogin="canLogin" :canRegister="canRegister" />
-        <main class="container py-8 mx-auto space-y-10">
-            <section class="px-20 space-y-4">
-                <div class="w-full mx-auto overflow-hidden text-center rounded-lg h-96">
-                    <img :src="service.thumbnail" :alt="service.name" class="object-cover">
+
+        <div
+            class="container flex items-start min-h-screen py-8 mx-auto space-y-10"
+        >
+            <main>
+                <section class="px-20 space-y-4">
+                    <div
+                        class="w-full mx-auto overflow-hidden text-center rounded-lg h-96"
+                    >
+                        <img
+                            :src="service.thumbnail"
+                            :alt="service.name"
+                            class="object-cover"
+                        />
+                    </div>
+
+                    <h1 class="text-3xl font-bold">
+                        {{ service.name }}
+                    </h1>
+
+                    <p class="leading-relaxed text-gray-600 whitespace-normal">
+                        {{ service.description }}
+                    </p>
+                </section>
+            </main>
+
+            <aside class="w-72">
+                <h2 class="mb-5 text-xl font-black">Other Services</h2>
+
+                <div class="space-y-4">
+                    <div v-for="n in 3">
+                        <div class="w-full h-40 mb-2 bg-gray-500 rounded-lg">
+                            <a href="#" class="block w-full h-full"></a>
+                        </div>
+                        <div>
+                            <a href="#" class="text-lg hover:underline"
+                                >Service type</a
+                            >
+                        </div>
+                    </div>
                 </div>
-
-                <h1 class="text-3xl font-bold">
-                    {{ service.name }}
-                </h1>
-
-                <p class="leading-relaxed text-gray-600">
-                    {{ service.description }}
-                </p>
-            </section>
-        </main>
+            </aside>
+        </div>
         <Footer />
     </div>
 </template>
