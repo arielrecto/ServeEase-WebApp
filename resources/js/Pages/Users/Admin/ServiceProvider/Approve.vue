b093bb358ae500de5a4ce7600d12c86764ba43c8
shuraiken <realninjadude@gmail.com>
2024-12-22 15:36:48 +0800

    service provider application approval

--- resources/js/Pages/Users/Admin/ServiceProvider/Approve.vue
+++ resources/js/Pages/Users/Admin/ServiceProvider/Approve.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { Modal } from '@inertiaui/modal-vue';
-import { router, useForm } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { Modal } from "@inertiaui/modal-vue";
+import { router, useForm } from "@inertiajs/vue3";
+import { ref } from "vue";
 
-defineProps({
+const props = defineProps({
     providerProfile: Object,
 });
 
@@ -12,29 +12,48 @@ const form = useForm({
 });
 
 const submit = () => {
-    form.approve(`/admin/service-provider/approved/${providerProfile.id}`, {
-        onFinish: visit => {
-            modalRef.value.emit('success');
+    form.put(`/admin/service-provider/approved/${props.providerProfile.id}`, {
+        onFinish: (visit) => {
+            modalRef.value.emit("success");
             modalRef.value.close();
         },
     });
-}
+};
 
 const modalRef = ref(null);
 </script>
 
 <template>
     <Modal ref="modalRef">
-        <div class="bg-white space-y-6 px-6 py-4 overflow-hidden rounded-md shadow">
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">Approve this application?</h2>
+        <div
+            class="px-6 py-4 space-y-6 overflow-hidden bg-white rounded-md shadow"
+        >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Approve this application?
+            </h2>
 
-            <p>Approving this user means they are an official service provider for ServEase.</p>
+            <p>
+                Approving this user means they are an official service provider
+                for ServEase.
+            </p>
 
             <form @submit.prevent="submit">
                 <div class="flex justify-end">
                     <div class="flex gap-x-4">
-                        <button @click="modalRef.close" type="button" class="button-ghost">Cancel</button>
-                        <button type="submit" :disabled="form.processing" class="button-primary">Confirm</button>
+                        <button
+                            @click="modalRef.close"
+                            type="button"
+                            class="button-ghost"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            :disabled="form.processing"
+                            class="button-primary"
+                        >
+                            Confirm
+                        </button>
                     </div>
                 </div>
             </form>
--- resources/js/Pages/Users/Admin/ServiceProvider/Index.vue
+++ resources/js/Pages/Users/Admin/ServiceProvider/Index.vue
@@ -1,61 +1,62 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import TableWrapper from '@/Components/Table/TableWrapper.vue';
-import TableHeader from '@/Components/Table/TableHeader.vue';
-import TableBody from '@/Components/Table/TableBody.vue';
-import ActionButton from '@/Components/ActionButton.vue';
-import { ModalLink } from '@inertiaui/modal-vue';
-import { Link } from '@inertiajs/vue3';
-import moment from 'moment';
-import { ref } from 'vue';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import TableWrapper from "@/Components/Table/TableWrapper.vue";
+import TableHeader from "@/Components/Table/TableHeader.vue";
+import TableBody from "@/Components/Table/TableBody.vue";
+import ActionButton from "@/Components/ActionButton.vue";
+import { ModalLink } from "@inertiaui/modal-vue";
+import { Link } from "@inertiajs/vue3";
+import moment from "moment";
+import { ref } from "vue";
 
 defineProps({
     providers: Object,
 });
 
 const headers = ref([
-    'Name',
-    'Service',
-    'Years of Experience',
-    'Date Submitted',
-    'Action',
+    "Name",
+    "Service",
+    "Years of Experience",
+    "Date Submitted",
+    "Action",
 ]);
 
 const items = ref([
     {
         id: 1,
-        name: 'David Rondina',
-        service_type: 'Plumbing',
-        experience: '2 years',
-        status: 'Pending',
-        created_at: 'Oct. 10, 2024',
+        name: "David Rondina",
+        service_type: "Plumbing",
+        experience: "2 years",
+        status: "Pending",
+        created_at: "Oct. 10, 2024",
     },
     {
         id: 2,
-        name: 'John Doe',
-        service_type: 'Plumbing',
-        experience: '2 years',
-        status: 'Pending',
-        created_at: 'Oct. 10, 2024',
+        name: "John Doe",
+        service_type: "Plumbing",
+        experience: "2 years",
+        status: "Pending",
+        created_at: "Oct. 10, 2024",
     },
     {
         id: 3,
-        name: 'Laufey Lin',
-        service_type: 'Plumbing',
-        experience: '4 years',
-        status: 'Pending',
-        created_at: 'Oct. 10, 2024',
+        name: "Laufey Lin",
+        service_type: "Plumbing",
+        experience: "4 years",
+        status: "Pending",
+        created_at: "Oct. 10, 2024",
     },
-])
+]);
 </script>
 
 <template>
-
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">Applications</h2>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Applications
+            </h2>
         </template>
 
         <div class="py-12">
@@ -78,21 +79,64 @@ const items = ref([
                                 </TableBody> -->
                                 <template v-if="providers.data.length !== 0">
                                     <tr v-for="provider in providers.data">
-                                        <td>{{ provider.profile.user.name }}</td>
+                                        <td>
+                                            {{ provider.profile.user.name }}
+                                        </td>
                                         <td>{{ provider.service_type }}</td>
                                         <td>{{ provider.experience }}</td>
-                                        <td>{{ moment(provider.created_at).format('ll') }}</td>
+                                        <td>
+                                            {{
+                                                moment(
+                                                    provider.created_at
+                                                ).format("ll")
+                                            }}
+                                        </td>
                                         <td>
                                             <div class="flex gap-x-4">
-                                                <ActionButton type="modal" actionType="view" :href="route('admin.service-provider.show', provider.id)" :modalSlideoverEnabled="true" />
-                                                <ActionButton type="modal" actionType="delete" :href="route('admin.service-provider.delete', provider.id)"/>
+                                                <ActionButton
+                                                    type="modal"
+                                                    actionType="view"
+                                                    :href="
+                                                        route(
+                                                            'admin.service-provider.show',
+                                                            provider.id
+                                                        )
+                                                    "
+                                                    :modalSlideoverEnabled="
+                                                        true
+                                                    "
+                                                />
+                                                <ActionButton
+                                                    type="modal"
+                                                    actionType="approve"
+                                                    :href="
+                                                        route(
+                                                            'admin.service-provider.approve',
+                                                            provider.id
+                                                        )
+                                                    "
+                                                />
+                                                <ActionButton
+                                                    type="modal"
+                                                    actionType="delete"
+                                                    :href="
+                                                        route(
+                                                            'admin.service-provider.delete',
+                                                            provider.id
+                                                        )
+                                                    "
+                                                />
                                             </div>
                                         </td>
                                     </tr>
                                 </template>
                                 <template v-else>
                                     <tr>
-                                        <td :colspan="headers.length"><p class="italic text-center">No records found.</p></td>
+                                        <td :colspan="headers.length">
+                                            <p class="italic text-center">
+                                                No records found.
+                                            </p>
+                                        </td>
                                     </tr>
                                 </template>
                             </template>
@@ -103,4 +147,3 @@ const items = ref([
         </div>
     </AuthenticatedLayout>
 </template>
-
