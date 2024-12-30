3f1cc903fc5dd9414dbd9413907c2f68968a1d1f
shuraiken <realninjadude@gmail.com>
2024-12-30 16:50:44 +0800

    update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use App\Models\ProviderProfile;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -52,7 +53,7 @@ class User extends Authenticatable
             return false;
         }
 
-        return Profile::whereId($profile->id)->has('providerProfile')->exists();
+        return ProviderProfile::whereProfileId($profile->id)->exists();
     }
 
     public function hasVerifiedProviderProfile()
