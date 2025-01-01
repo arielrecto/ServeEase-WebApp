02814925f660c7171a609d0f42e5f903a320365f
shuraiken <realninjadude@gmail.com>
2025-01-01 17:48:18 +0800

    update DropdownLink.vue

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -1,19 +1,32 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import ModalLinkDialog from "./Modal/ModalLinkDialog.vue";
+import { Link } from "@inertiajs/vue3";
 
 defineProps({
     href: {
         type: String,
         required: true,
     },
+    isModalLink: {
+        type: Boolean,
+        default: false,
+    },
 });
 </script>
 
 <template>
     <Link
+        v-if="!isModalLink"
         :href="href"
         class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
     >
         <slot />
     </Link>
+    <ModalLinkDialog
+        v-else
+        :href="href"
+        class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+    >
+        <slot />
+    </ModalLinkDialog>
 </template>
