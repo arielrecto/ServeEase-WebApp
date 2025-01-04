d06d5b0de4defcf60ad42b9d1d9da249d1b5b07a
shuraiken <realninjadude@gmail.com>
2025-01-04 22:52:39 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Inertia\Inertia;
+use App\Models\ServiceType;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\HomeController;
@@ -14,10 +15,12 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ServiceTypeController;
 use App\Http\Controllers\ServiceProvider\ServiceController;
 use App\Http\Controllers\Customer\CustomerFeedbackController;
+use App\Http\Controllers\Admin\ServiceProviderApplicationController;
 use App\Http\Controllers\Admin\ServiceProviderController as AdminSPController;
 use App\Http\Controllers\Customer\ServiceController as CustomerServiceController;
 use App\Http\Controllers\Customer\ServiceProviderController as CustomerSPController;
 use App\Http\Controllers\Customer\DashboardController as CustomerDashboardController;
+use App\Http\Controllers\Admin\CustomerFeedbackController as ADCustomerFeedbackController;
 use App\Http\Controllers\ServiceProvider\DashboardController as ServiceProviderDashboardController;
 
 
@@ -76,9 +79,17 @@ Route::middleware('auth')->group(function () {
             Route::put('/approved/{id}', [AdminSPController::class, 'approved'])->name('approved');
             Route::get('/delete/{id}', [AdminSPController::class, 'delete'])->name('delete');
         });
+        // Route::prefix('applications')->as('applications.')->controller(ServiceProviderApplicationController::class)->group(function () {
+        //     Route::get('/approve/{id}', 'approve')->name('approve');
+        //     Route::put('/approved/{id}', 'approved')->name('approved');
+        //     Route::get('/delete/{id}', 'delete')->name('delete');
+        // });
+        Route::resource('applications', ServiceProviderApplicationController::class)->except(["create", "store", "edit", "update"]);
         Route::resource('service-types', ServiceTypeController::class)->except('update');
         Route::post('/service-types/{id}', [ServiceTypeController::class, 'update'])->name('service-types.update');
 
+        Route::get('/feedbacks/delete/{feedback}', [ADCustomerFeedbackController::class, 'delete'])->name('feedbacks.delete');
+        Route::resource('feedbacks', ADCustomerFeedbackController::class)->except(['create', 'store']);
         Route::resource('barangays', BarangayController::class)->except(['show']);
         Route::get('/barangays/delete/{id}', [BarangayController::class, 'delete'])->name('barangays.delete');
 
