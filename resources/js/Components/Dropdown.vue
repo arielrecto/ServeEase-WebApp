0f9883f9bb631267b117d24eab0888b7584acfcf
davidrondina <rondindavid.r@gmail.com>
2025-03-16 17:37:20 +0800

    add emits to Dropdown.vue

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -16,6 +16,8 @@ const props = defineProps({
     },
 });
 
+const emits = defineEmits(["dropdownOpen"]);
+
 const closeOnEscape = (e) => {
     if (open.value && e.key === "Escape") {
         open.value = false;
@@ -43,12 +45,19 @@ const alignmentClasses = computed(() => {
     }
 });
 
+const toggleDropdown = () => {
+    open.value = !open.value;
+    if (open.value) {
+        emits("dropdownOpen");
+    }
+};
+
 const open = ref(false);
 </script>
 
 <template>
     <div class="relative">
-        <div @click="open = !open">
+        <div @click="toggleDropdown">
             <slot name="trigger" />
         </div>
 
@@ -60,12 +69,12 @@ const open = ref(false);
         ></div>
 
         <Transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="opacity-0 scale-95"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-95"
+            enter-active-class="transition duration-200 ease-out"
+            enter-from-class="scale-95 opacity-0"
+            enter-to-class="scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="scale-100 opacity-100"
+            leave-to-class="scale-95 opacity-0"
         >
             <div
                 v-show="open"
