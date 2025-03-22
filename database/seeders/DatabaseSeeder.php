aae3aff12cb0fddcc71b7be2e9bd6e998894cac1
davidrondina <rondindavid.r@gmail.com>
2025-03-22 14:53:24 +0800

    disable some seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -56,12 +56,12 @@ class DatabaseSeeder extends Seeder
         //     ]);
         // });
 
-        $this->call([
-            ServiceTypeSeeder::class,
-            BarangaySeeder::class,
-            UserProviderSeeder::class,
-            AvailServiceSeeder::class,
-            FeedbackSeeder::class,
-        ]);
+        // $this->call([
+        //     ServiceTypeSeeder::class,
+        //     BarangaySeeder::class,
+        //     UserProviderSeeder::class,
+        //     AvailServiceSeeder::class,
+        //     FeedbackSeeder::class,
+        // ]);
     }
 }
