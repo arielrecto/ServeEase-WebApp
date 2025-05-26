5be57c18fb703e961ba37d7e3dc347905c5ca898
shuraiken <realninjadude@gmail.com>
2025-05-27 07:29:40 +0800

    fix payment accounts page: alignment issue in page header

--- resources/js/Pages/Users/ServiceProvider/PaymentAccount/Index.vue
+++ resources/js/Pages/Users/ServiceProvider/PaymentAccount/Index.vue
@@ -130,7 +130,7 @@ const deleteAccount = (accountId) => {
 
     <AuthenticatedLayout>
         <template #header>
-            <div class="flex items-center justify-between">
+            <div class="w-full flex items-center justify-between">
                 <h2 class="text-xl font-semibold leading-tight text-gray-800">
                     Payment Accounts
                 </h2>
