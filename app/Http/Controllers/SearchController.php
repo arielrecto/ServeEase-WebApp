5b1daec17e95af9329992745e0a04642f20e0834
David Rondina <109362396+davidrondina@users.noreply.github.com>
2025-06-11 13:13:22 +0800

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -42,14 +42,13 @@ class SearchController extends Controller
                 // $request->service = null;
                 $request->brgy = null;
 
-                $q->where('name', 'LIKE', "%{$request->search}%")
-                    ->where('service_type_id', $request->service)
-                    ->orWhere(function ($q) use ($request) {
-                        $q->whereHas('user.profile', function ($q) use ($request) {
+                $q->where(function ($q) use ($request) {
+                    $q->where('name', 'LIKE', "%{$request->search}%")
+                        ->orWhereHas('user.profile', function ($q) use ($request) {
                             $q->where('first_name', 'LIKE', "%{$request->search}%")
                                 ->orWhere('last_name', 'LIKE', "%{$request->search}%");
                         });
-                    });
+                });
             })
             ->when($request->brgy, function ($q) use ($request) {
                 $q->where('barangay_id', $request->brgy);
